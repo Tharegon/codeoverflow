aff8cd2592d51fcab4e1bb2d654a78161dfbcfe8
Tharegon <dombovari11david@gmaa>
2021-05-06 14:22:50 +0200

    Initial commit

---
+++ src/main/java/com/codecool/codeoverflow/CodeoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.codeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeoverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeoverflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/codeoverflow/CodeoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.codeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeoverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
