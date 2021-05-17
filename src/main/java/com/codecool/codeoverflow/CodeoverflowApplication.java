704221d284affd47d7db23f694ce086af6ab3ac3
Tharegon <dombovari11david@gmaa>
2021-05-17 15:38:49 +0200

    Add data to dummy Database

--- src/main/java/com/codecool/codeoverflow/CodeoverflowApplication.java
+++ src/main/java/com/codecool/codeoverflow/CodeoverflowApplication.java
@@ -1,5 +1,8 @@
 package com.codecool.codeoverflow;
 
+import com.codecool.codeoverflow.entity.Question;
+import com.codecool.codeoverflow.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +12,24 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication
 public class CodeoverflowApplication {
 
+    public CodeoverflowApplication(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CodeoverflowApplication.class, args);
     }
 
+    final QuestionRepository questionRepository;
 
     @Bean
     @Profile("production")
     public CommandLineRunner init() {
         return args -> {
+            Question first = new Question((long) 1,"Why lombok doesnt work?","very good explanation");
+
+            questionRepository.save(first);
+
         };
     }
 }
