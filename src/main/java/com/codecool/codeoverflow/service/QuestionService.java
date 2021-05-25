a9b195fe833158458e7af7050872eb623c2f209f
Tharegon <dombovari11david@gmaa>
2021-05-25 13:49:56 +0200

    Add saveQuestion

--- src/main/java/com/codecool/codeoverflow/service/QuestionService.java
+++ src/main/java/com/codecool/codeoverflow/service/QuestionService.java
@@ -2,11 +2,15 @@ package com.codecool.codeoverflow.service;
 
 
 import com.codecool.codeoverflow.entity.Question;
+import com.codecool.codeoverflow.model.QuestionCredentials;
 import com.codecool.codeoverflow.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class QuestionService {
@@ -19,5 +23,16 @@ public class QuestionService {
 
     public List<Question> getAllQuestion(){
         return questionRepository.findAll();
+    }
+
+    public ResponseEntity saveQuestion(QuestionCredentials questionCredentials) {
+        Question question = Question.builder()
+                .questionTitle(questionCredentials.getQuestionTitle())
+                .questionContent(questionCredentials.getQuestionContent())
+                .build();
+        questionRepository.save(question);
+        Map<Object, Object> model = new HashMap<>();
+        model.put("Successful question with: ", question.getQuestionTitle());
+        return ResponseEntity.ok(model);
     }
 }
